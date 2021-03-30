ad40a83cdc0e56b8ae643cb1d49df27d2e926309
Wilfredo Hernández <wilfredo.hernandez.ar@gmail.com>
2021-03-30 15:26:30 +0200

    Agregados Return Types, responsive funciona

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { Bars } from "../types";
 import { getRandomInt } from "../utils/getRandomInt";
 
-const CANVAS_SIZE = 800;
+
 const barWidth = 35;
 const MAX_BAR_QUANTITY = 20;
 const MIN_BAR_QUANTITY = 4;
@@ -33,4 +33,4 @@ const INITIAL_BAR_ARR: Bars = [
     },
 ];
 
-export { CANVAS_SIZE, barWidth, MAX_BAR_QUANTITY, MIN_BAR_QUANTITY, INITIAL_BAR_ARR };
+export { barWidth, MAX_BAR_QUANTITY, MIN_BAR_QUANTITY, INITIAL_BAR_ARR };

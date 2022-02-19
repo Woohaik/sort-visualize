c7dde46cc9a9cf211c41da540dbdf6055bde36be
Wilfredo Hernández <74992119+Woohaik@users.noreply.github.com>
2022-02-19 17:37:27 +0100

    Updated LinkedIn URL

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -3,10 +3,10 @@ import { FC } from "react";
 import gitHubLogo from "./../assets/github.png";
 import linkedInLogo from "./../assets/linkedin.svg";
 
-const social: FC = () => {
+const Social: FC = () => {
     return (
         <div className="social">
-            <a className="item" href="https://www.linkedin.com/in/wilfredo-oswaldo-hern%C3%A1ndez-argueta-210108204/"  >
+            <a className="item" href="https://www.linkedin.com/in/wilfredo-hndz"  >
                 <img src={linkedInLogo} alt="LinkdIn Logo" />
                 <div className="fs-12 fc-transparent">
                     Wilfredo Hernández
@@ -22,4 +22,4 @@ const social: FC = () => {
         </div>
     );
 };
-export default social;
+export default Social;
